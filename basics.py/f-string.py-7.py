f77ccc76049ccc33e58b45670f09d78ddf4cda4d
Nili-byte <nihalmanihar1234@gmail.com>
2026-06-01 03:01:35 +0530

    f string

---
+++ basics.py/f-string.py-7.py
@@ -0,0 +1,18 @@
+a = ("hello my name is {} and im from {}")
+
+
+n = "nihal"
+c = "india"
+
+print(a.format(n,c))
+print(a.format(c,n))
+
+a = ("hello my name is {0} and im from {1}")
+print(a.format(c,n))
+p = "python"
+c = "cool"
+print(f"hello guys i'm learning {p} and its {c}")
+
+m=0.78668
+print(f"{m:.3f}")
+
