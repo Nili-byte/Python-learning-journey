0424323ee4216c0ae570969b7887f5f72cfd26c5
Nili-byte <nihalmanihar1234@gmail.com>
2026-05-31 17:56:01 +0530

    KBC

---
+++ projects/KBC.py
@@ -0,0 +1,19 @@
+h = "---Welcome to Kon Banega Crorepati---"
+
+print(h.center(50))
+
+n = (input("Enter your name :  "))
+
+print("What is the national bird of India")
+ans = (input())
+
+
+a1 = ["peacock"]
+
+
+if (ans==a1):
+    print("Congrats you have won ")
+
+else:
+    print("You lost")
+
