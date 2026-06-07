3ee040ecdc3c6a6b02606a5515bcd0fe0fbd1066
Nili-byte <nihalmanihar1234@gmail.com>
2026-06-08 00:56:02 +0530

    shortIfelse_enumerate

---
+++ basics.py/shortifelse_enumerate-D11.py
@@ -0,0 +1,46 @@
+  
+# while True:
+
+#  n  = int(input("Enter a number (0-10)"))
+#  a = n**n
+#  print("Invalid ")if n<0 or n>10 else print(a)
+
+ 
+
+
+
+
+
+# a = [1,2,3,4,5]
+# for i,index in enumerate(a,start=1):
+#     print(i)
+#     if(i==3):
+#         print("Go")
+
+
+b = ["first","secomd","third"]
+
+for idx,i in enumerate(b, start=1):
+    print(idx," = ",i)
+    
+
+
+c=["cricket","hockey","ufc","chess"]
+idi = 0
+for s in c:
+    print(s)                 #normal life
+    if(idi == 2):
+        print("Here we go again")
+    idi+=1
+
+print("="*30)
+
+d=["cricket","hockey","ufc","chess"]
+for index,u in enumerate(d):
+    print(u)                 #mentos life
+    if(index == 2):
+     print("Here we go again")
+
+   
+    
+ 
