b3e7ba4a3217503ef877b7560e39101561a7e2d8
Nili-byte <nihalmanihar1234@gmail.com>
2026-08-19 14:25:20 +0530

    Password_generator

---
+++ projects/Mini_projects.py/Password_generator.py
@@ -0,0 +1,33 @@
+import random
+import string
+
+lower = list(string.ascii_lowercase)
+
+upper = list(string.ascii_uppercase)
+
+digits = list(str(string.digits))
+
+spec_char = list(string.punctuation)
+
+
+
+wl = lower + upper + spec_char + digits
+password = ""
+while True:
+    try:
+            usrlen = int(input(("Enter the length of your password : ")))
+            break
+    except(ValueError):
+          print("Invalid! Try again : ")
+
+for i in range(usrlen):
+    i = random.choice(wl)
+    password = password + i
+
+print("Your Password has been generated -", password)    
+      
+
+
+
+
+    
